15b3addd0f457a2f428b4a3e5bb459e28594f725
SeasonedSoup <emmetteagoncillo1@gmail.com>
2025-10-21 18:43:44 +0800

    Add details.ejs for seeing detail of the message

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const path = require("path")
-const router = require("./routes/formrouter")
+
 const PORT = 8000;
 const messages = [
     {
@@ -14,19 +14,27 @@ const messages = [
         added: new Date()
     },
 ];
+
+const router = require("./routes/formrouter")(messages)
 const app = express();
 //set view engine
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs")
 
 //parses form data to req.body or sum
-app.use("/new", router)
 app.use(express.urlencoded({ extended: true }));
+app.use("/new", router)
+
 
 //app
 app.get("/", (req, res) => {
   res.render("index", { title: "Mini Messageboard", messages: messages })
 });
+
+app.get("/messages/:id", (req, res) => {
+    const message = messages[req.params.id]
+    res.render("details", {message})
+})
 
 app.listen(PORT, (error) => {
     if(error) {
--- routes/formrouter.js
+++ routes/formrouter.js
@@ -1,15 +1,16 @@
 const {Router} = require("express")
-
 const router = Router()
 
-router.get("/new", (req, res) => {
-    res.render("form")
-})
-
-router.post("/new", (req, res) => {
-    const messageUser = req.body.authorName
-    const messageText = req.body.message
-    messages.push({ text: messageText, user: messageUser, added: new Date() });
-    res.redirect("/")
-})
-module.exports = router;
+module.exports = (messages) => {
+    router.get("/", (req, res) => {
+        res.render("form")
+    })
+    
+    router.post("/", (req, res) => {
+        const messageUser = req.body.authorName
+        const messageText = req.body.message
+        messages.push({ text: messageText, user: messageUser, added: new Date() });
+        res.redirect("/")
+    })
+    return router
+} ;
